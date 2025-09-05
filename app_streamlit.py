8f46a58f063919a31dc56f4760131f3e6cb6c089
eulopez <eulopez@GRUPOLASEGUNDA.COM.AR>
2025-09-05 11:11:18 -0300

    chequeo-cambios-rutas

--- app_streamlit.py
+++ app_streamlit.py
@@ -14,17 +14,17 @@ try:
     df_long = pd.read_csv('df_long.csv')
 
     # dfs de repuestos orion/cesvi
-    df_tipo_rep = pd.read_csv('data/df_tipo_rep.csv')
-    df_rep_tv = pd.read_csv('data/df_rep_tv.csv')
+    df_tipo_rep = pd.read_csv('df_tipo_rep.csv')
+    df_rep_tv = pd.read_csv('df_rep_tv.csv')
 
     # dfs de mano de obra orion/cesvi
-    df_cm_mo_hist = pd.read_csv('data/df_cm_mo_hist.csv')
-    df_cm_mo_ipc = pd.read_csv('data/df_cm_mo_ipc.csv')
-    df_cm_mo_usd = pd.read_csv('data/df_cm_mo_usd.csv')
+    df_cm_mo_hist = pd.read_csv('df_cm_mo_hist.csv')
+    df_cm_mo_ipc = pd.read_csv('df_cm_mo_ipc.csv')
+    df_cm_mo_usd = pd.read_csv('df_cm_mo_usd.csv')
     # dfs mano de obra cleas si/cleas no
-    df_cm_mo_hist_cleas = pd.read_csv('data/df_cm_mo_hist_cleas.csv')
-    df_cm_mo_ipc_cleas = pd.read_csv('data/df_cm_mo_ipc_cleas.csv')
-    df_cm_mo_usd_cleas = pd.read_csv('data/df_cm_mo_usd_cleas.csv')
+    df_cm_mo_hist_cleas = pd.read_csv('df_cm_mo_hist_cleas.csv')
+    df_cm_mo_ipc_cleas = pd.read_csv('df_cm_mo_ipc_cleas.csv')
+    df_cm_mo_usd_cleas = pd.read_csv('df_cm_mo_usd_cleas.csv')
 
     # dfs marcas
     df_marcas_autos = pd.read_csv('data/evol_todas_marcas_autos.csv')
